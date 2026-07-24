e399cde089520f3673604ad20627a4571cee7a68
Fülöp Martin <fulop.martin21@gmail.com>
2026-07-24 16:06:32 +0200

    Init repository

---
+++ src/test/java/org/portfolio/java/portfolio_work/PortfolioWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.portfolio.java.portfolio_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
